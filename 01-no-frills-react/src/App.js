4ab329fe33b0762d60c62895b6fb4ff8ac14035d
SalehMohammed01 <94100081+SalehMohammed01@users.noreply.github.com>
2022-04-25 11:23:18 +0530

    first section

---
+++ 01-no-frills-react/src/App.js
@@ -0,0 +1,20 @@
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h2', {}, props.name),
+        React.createElement('h3', {}, props.animal),
+        React.createElement('h3', {}, props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        'div',
+        {},
+            React.createElement('h1', {id: 'my-brand'}, 'Adopt me!'),
+            React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanese'}),
+            React.createElement(Pet, { name: 'Pepper', animal: 'Bird', breed: 'cockatiel' }),
+            React.createElement(Pet, { name: 'Sudo', animal: 'Dog', breed: 'Wheaten Terrier' }),
+    );
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
